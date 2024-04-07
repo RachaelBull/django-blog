89922425bea0e38050a96e340c4ca52639c7cffb
Rachael Bull <rachaelbull2021@gmail.com>
2024-04-07 17:47:05 +0000

    Add the about layout - need to add content

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,9 +27,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-rachaelbull-djangoblog-jbvf6vahlbe.ws-eu110.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-rachaelbull-djangoblog-jbvf6vahlbe.ws-us110.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_summernote',
     'blog',
+    'about',
 ]
 
 MIDDLEWARE = [
